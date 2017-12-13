e22d0807d8939b45817b203de445c0a588277292
AsianDave94 <Davidtjmalmberg@gmail.com>
2017-12-13 12:42:28 +0100

    Update

--- 2SemesterProjekt/DomainMasterDetailsViewModel/BookingMasterDetailsViewModel.cs
+++ 2SemesterProjekt/DomainMasterDetailsViewModel/BookingMasterDetailsViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using ExtensionsCommands.Types;
 using ExtensionsViewModel.Implementation;
 
 namespace _2SemesterProjekt.BookingFolder
@@ -13,7 +14,9 @@ namespace _2SemesterProjekt.BookingFolder
         public BookingMasterDetailsViewModel()
             : base(new BookingVmFactory(), ObjectProvider.BookingCatalog,
                 new List<string> { }, new List<string>())
-        { }
+        {
+            ViewStateService.ViewState = CRUDStates.CreateState;
+        }
 
 
 
